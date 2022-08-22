a48e8e4df36ffa0ab3d53e80464e7567ad3bb556
goldebrodkk <jake.kelly3564@gmail.com>
2022-08-22 09:56:11 +0100

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express'); 
+const cors = require('cors'); 
 const { getTopics } = require('./controllers/topic-controller'); 
 const { getUsers,
         getUserByUsername } = require('./controllers/user-controller')
@@ -16,6 +17,8 @@ const endpoints = require('./endpoints.json');
 
 
 const app = express(); 
+
+app.use(cors()); 
 
 app.use(express.json()); 
 
